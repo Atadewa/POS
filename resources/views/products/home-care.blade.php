d2c0cf99653397b1d4b99cf4381cde989a02f9c8
Atadewa <aditafli42@gmail.com>
2025-02-18 18:01:29 +0700

    feat: add home care category view

---
+++ resources/views/products/home-care.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Produk Perawatan Rumah</title>
+</head>
+<body>
+    <h1>Perawatan Rumah</h1>
+    <ul>
+        <li>Sapu</li>
+        <li>Deterjen</li>
+        <li>Pembersih kaca</li>
+    </ul>
+</body>
+</html>
