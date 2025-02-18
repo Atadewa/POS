20aa54dce9fed61b055d3763501731ae95bc8008
Atadewa <aditafli42@gmail.com>
2025-02-18 18:01:49 +0700

    feat: add user profile view

---
+++ resources/views/user/profile.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Profil Pengguna</title>
+</head>
+<body>
+    <h1>Profil Pengguna</h1>
+    <p>ID Pengguna: {{ $id }}</p>
+    <p>Nama: {{ $name }}</p>
+</body>
+</html>
