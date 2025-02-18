597fc0649d9da4c25328cac09dc92f8a282162b9
Atadewa <aditafli42@gmail.com>
2025-02-18 18:01:42 +0700

    feat: add baby and kids category view

---
+++ resources/views/products/baby-kid.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Produk Bayi dan Anak</title>
+</head>
+<body>
+    <h1>Bayi dan Anak</h1>
+    <ul>
+        <li>Popok</li>
+        <li>Mainan</li>
+        <li>Pakaian Anak</li>
+    </ul>
+</body>
+</html>
