58f2eae8e3645d0ee6599ed2e13b4f6a29cf260a
Atadewa <aditafli42@gmail.com>
2025-02-18 18:00:36 +0700

    feat: add homepage view

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Utama</title>
+</head>
+<body>
+    <h1>Selamat Datang di Website Kami</h1>
+</body>
+</html>
