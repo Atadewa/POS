0b344625c21f4333e597d19296c3285465c6c562
Atadewa <aditafli42@gmail.com>
2025-02-18 18:01:19 +0700

    feat: add beauty and health category view

---
+++ resources/views/products/beauty-health.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Produk Kecantikan dan Kesehatan</title>
+</head>
+<body>
+    <h1>Kecantikan dan Kesehatan</h1>
+    <ul>
+        <li>Sabun muka</li>
+        <li>Sampo</li>
+        <li>Suplemen Vitamin</li>
+    </ul>
+</body>
+</html>
